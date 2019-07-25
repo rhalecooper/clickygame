5fae7b1162821e83c82b4f3d8a900a2bce968e99
rhalecooper <rhalecooper@gmail.com>
2019-07-25 02:16:15 -0400

    Updates

---
+++ precache-manifest.043ba4a3ea67f5f12c875e51e6252ec9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc65f7080b2a758a3f8fe2021af6362",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "7478aa69d5ac0a347bb7",
+    "url": "/clickygame/static/css/main.3f871991.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "7478aa69d5ac0a347bb7",
+    "url": "/clickygame/static/js/main.baea4915.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
